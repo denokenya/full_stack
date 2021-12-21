19ecb3a9a727a16c78cb6dc46380a87fa3ab7b6e
denokenya <dennismuteti25@gmail.com>
2021-12-21 12:08:51 +0300

    create database connection

--- config/database.js
+++ config/database.js
@@ -1,2 +1,11 @@
 //import sequalize
-import {} from "sequalize";
+import {Sequelize} from "sequelize";
+
+//database connection
+
+const db = new Sequelize('mern_db',root,'',{
+    host: "localhost",
+    dialect: "mysql"
+});
+
+export default db;
